27298aa7e0c81cc63ccf9b76535034bcd0510e34
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2025-05-03 20:33:18 +0100

    updating Giants.py

--- Giants.py
+++ Giants.py
@@ -43,9 +43,9 @@ def get_giants_json():
 
     for j in evolutions.find_all("tr")[1:]:
         row_data = j.find_all("td")
-        cost = row_data[7].contents[0]
+        cost = row_data[8].contents[0]
         row = [re.sub(r'\(.*\)', '', re.sub(r'\[.*\]', '', i.text.replace("\n", ""))) for i in row_data]
-        row[7] = cost
+        row[8] = cost
         length = len(evolutions_data)
         evolutions_data.loc[length] = row
 
